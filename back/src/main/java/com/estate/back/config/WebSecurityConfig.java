444abe45116b5516577b0c77f5a07f81f7ccfb78
Kim_dain <ekdls15937@naver.com>
2024-04-30 10:51:09 +0900

    2024.04.30 10:51

--- back/src/main/java/com/estate/back/config/WebSecurityConfig.java
+++ back/src/main/java/com/estate/back/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -15,6 +16,8 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.estate.back.filter.JwtAuthenticationFilter;
+import com.estate.back.handler.OAuth2SuccessHandler;
+import com.estate.back.service.implimentation.OAuth2UserServiceImplementation;
 
 import lombok.RequiredArgsConstructor;
 
@@ -31,6 +34,8 @@ import lombok.RequiredArgsConstructor;
 public class WebSecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final OAuth2UserServiceImplementation oAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
     
     @Bean
     protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
@@ -43,6 +48,12 @@ public class WebSecurityConfig {
             )
             .cors(cors -> cors
                 .configurationSource(corsConfigurationSource())
+            )
+            .oauth2Login(oauth2 -> oauth2
+                .authorizationEndpoint(endpoint -> endpoint.baseUri("/api/v1/auth/oauth2"))
+                .redirectionEndpoint(endpoint -> endpoint.baseUri("/oauth2/callback/*"))
+                .userInfoEndpoint(endpoint -> endpoint.userService(oAuth2UserService))
+                .successHandler(oAuth2SuccessHandler)
             )
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
