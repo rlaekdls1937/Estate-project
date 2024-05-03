54101209156204f04214d5b318b32410834866bb
Kim_dain <ekdls15937@naver.com>
2024-05-03 17:50:51 +0900

    2024.05.03 17:50

--- front/src/views/service/qna/QnaList/index.tsx
+++ front/src/views/service/qna/QnaList/index.tsx
@@ -1,13 +1,48 @@
-import React, { useEffect, useState } from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import './style.css'
 import { useUserStore } from 'src/stores';
 import { useNavigate } from 'react-router';
-import { AUTH_ABSOLUTE_PATH, COUNT_PER_PAGE, COUNT_PER_SECTION, QNA_WRITE_ABSOLUTE_PATH } from 'src/constant';
+import { AUTH_ABSOLUTE_PATH, COUNT_PER_PAGE, COUNT_PER_SECTION, QNA_DETAIL_ABSOLUTE_PATH, QNA_WRITE_ABSOLUTE_PATH } from 'src/constant';
 import { BoardListItem } from 'src/types';
-import { getBoardListRequest } from 'src/apis/board';
+import { getBoardListRequest, getSearchBoardListRequest } from 'src/apis/board';
 import { useCookies } from 'react-cookie';
-import { GetBoardListResponseDto } from 'src/apis/board/dto/response';
+import { GetBoardListResponseDto, GetSearchBoardListResponseDto } from 'src/apis/board/dto/response';
 import ResponseDto from 'src/apis/response.dto';
+
+//                      component                   //
+function ListItem ({
+    receptionNumber,
+    status,
+    title,
+    writerId,
+    writeDatetime,
+    viewCount
+}: BoardListItem) {
+    //                    function                //
+    const navigator = useNavigate();
+
+    //                    event handler                 //
+    const onClickHandler = () => navigator(QNA_DETAIL_ABSOLUTE_PATH(receptionNumber));
+
+    
+
+    //                    render                      //
+    return (
+        <div className='qna-list-table-tr' onClick={onClickHandler}>
+                <div className='qna-list-table-reception-number'>{receptionNumber}</div>
+                    <div className='qna-list-table-status'>
+                        {status ?
+                         <div className='disable-bedge'>완료</div> :
+                         <div className='primary-bedge'>접수</div>
+                        }
+                    </div>
+                    <div className='qna-list-table-title' style={{ textAlign:'left' }}>{title}</div>
+                    <div className='qna-list-table-writer-id'>{writerId}</div>
+                    <div className='qna-list-table-writer-date'>{writeDatetime}</div>
+                    <div className='qna-list-table-viewcount'>{viewCount}</div>
+                </div>
+    );
+}
 
 //                      component                   //
 export default function QnaList() {
@@ -27,25 +62,33 @@ export default function QnaList() {
     const [currentSection, setCurrentSection] = useState<number>(1);
     const [isToggleOn, setToggleOn] = useState<boolean>(false);
 
+    const [searchWord, setSearchWord] = useState<string>('');
+
     //                    function                       //
     const navigator = useNavigate();
 
-    const getBoardListResponse = (result: GetBoardListResponseDto | ResponseDto | null) => {
-        const message = 
-            !result ? '서버에 문제가 있습니다.' : 
-            result.code === 'AF' ? '인증에 실패했습니다.'  :
-            result.code === 'DBE' ? '서버에 문제가 있습니다.' : '';
-        if (!result || result.code ! == 'SU') {
-            alert(message)
-            if (result?.code === 'AF') navigator(AUTH_ABSOLUTE_PATH);
-            return;
-        }
+    const changePage = (boardList: BoardListItem[], totalLength: number) => {
+        const startIndex = (currentPage - 1) * COUNT_PER_PAGE;
+        let endIndex = currentPage * COUNT_PER_PAGE;
+        if (endIndex > totalLength - 1) endIndex = totalLength;
+        const viewList = boardList.slice(startIndex, endIndex);
+        setViewList(viewList);
+    };
 
-        const { boardList } = result as GetBoardListResponseDto;
+    const changeSection = (totalPage: number) => {
+        const startPage = (currentSection * COUNT_PER_SECTION) - (COUNT_PER_SECTION - 1);
+        let endPage = currentSection * COUNT_PER_SECTION;
+        if (endPage > totalPage) endPage = totalPage;
+        const pageList: number[] = [];
+        for (let page = startPage; page <= endPage; page++) pageList.push(page);
+        setPageList(pageList);
+    };
+
+    const changeBoardList = (boardList: BoardListItem[]) => {
         setBoardList(boardList);
 
         const totalLength = boardList.length;
-        setTotalLength(boardList.length);
+        setTotalLength(totalLength);
 
         const totalPage = Math.floor((totalLength - 1) / COUNT_PER_PAGE) + 1;
         setTotalPage(totalPage);
@@ -53,18 +96,46 @@ export default function QnaList() {
         const totalSection = Math.floor((totalPage - 1) / COUNT_PER_SECTION) + 1;
         setTotalSection(totalSection);
 
-        const startIndex = (currentPage - 1) * COUNT_PER_PAGE;
-        let endIndex = currentPage * COUNT_PER_PAGE;
-        if (endIndex > totalLength - 1) endIndex = totalLength - 1;
-        const viewList = boardList.slice(startIndex, endIndex);
-        setViewList(viewList);
+        changePage(boardList, totalLength);
 
-        const startPage = (currentSection * COUNT_PER_SECTION) - (COUNT_PER_SECTION - 1);
-        let endPage = currentSection * COUNT_PER_SECTION;
-        if (endPage > totalPage) endPage = totalPage;
-        const pageLsit: number[] = [];
-        for (let page = startPage; page <=endPage; page++) pageList.push(page);
-        setPageList(pageList);
+        changeSection(totalPage);
+    };
+
+    const getBoardListResponse = (result: GetBoardListResponseDto | ResponseDto | null) => {
+        const message = 
+            !result ? '서버에 문제가 있습니다.' : 
+            result.code === 'AF' ? '인증에 실패했습니다.'  :
+            result.code === 'DBE' ? '서버에 문제가 있습니다.' : '';
+        if (!result || result.code !== 'SU') {
+            alert(message);
+            if (result?.code === 'AF') navigator(AUTH_ABSOLUTE_PATH);
+            return;
+        }
+
+        const { boardList } = result as GetBoardListResponseDto;
+        changeBoardList(boardList);
+    };
+
+    const getSearchBoardListResponse = (result: GetSearchBoardListResponseDto | ResponseDto | null) => {
+
+        const message = 
+            !result ? '서버에 문제가 있습니다.' : 
+            result.code === 'VF' ? '검색어를 입력하세요.' :
+            result.code === 'AF' ? '인증에 실패했습니다.' :
+            result.code === 'DBE' ? '서버에 문제가 있습니다.' : '';
+        
+        if (!result || result.code !== 'SU') {
+            alert(message);
+            if (result?.code === 'AF') navigator(AUTH_ABSOLUTE_PATH);
+            return;
+        }
+
+        const { boardList } = result as GetSearchBoardListResponseDto;
+        changeBoardList(boardList);
+        setCurrentPage(1);
+        setCurrentSection(1);
+
+
     };
 
     //                    event handler                       //
@@ -78,16 +149,52 @@ export default function QnaList() {
         setToggleOn(!isToggleOn);
     };
 
+    const onPageClickHandler = (page: number) => {
+        setCurrentPage(page);
+    };
+
+    const onPreSectionClickHandler = () => {
+        if (currentSection === 1) return;
+        setCurrentSection(currentSection - 1); 
+        setCurrentPage((currentSection - 1) * COUNT_PER_SECTION);
+    };
+
+    const onNextSectionClickHandler = () => {
+        if (currentSection === totalSection) return;
+        setCurrentSection(currentSection + 1);
+        setCurrentPage(currentSection * COUNT_PER_SECTION + 1);
+    };
+
+    const onSearchWordChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const searchWord = event.target.value;
+        setSearchWord(searchWord);
+    };
+
+    const onSearchButtonClickHandler = () => {
+        if (!searchWord) return;
+        if (!cookies.accessToken) return;
+
+        getSearchBoardListRequest(searchWord, cookies.accessToken).then(getSearchBoardListResponse);
+    };
+
     //                  effect                  //
     useEffect(() => {
         if (!cookies.accessToken) return;
-        getBoardListRequest(cookies.accessToken).then(getBoardListResponse
-    
-        );
+        getBoardListRequest(cookies.accessToken).then(getBoardListResponse);
     },[]);
+
+    useEffect(() => {
+        changePage(boardList, totalLength);
+    },[currentPage]);
+
+    useEffect(() => {
+        if (!boardList.length) return;
+        changeSection(totalPage);
+    }, [currentSection]);
 
     //                    render                      //
     const toggleClass = isToggleOn ? 'toggle-active' : 'toggle';
+    const searchButtonClass = searchWord ? 'primary-button' : 'disable-button';
     return (
         <div id='qna-list-wrapper'>
             <div className='qna-list-top'>
@@ -111,32 +218,26 @@ export default function QnaList() {
                     <div className='qna-list-table-writer-date'>작성일</div>
                     <div className='qna-list-table-viewcount'>조회수</div>
                 </div>
-                <div className='qna-list-table-tr'>
-                <div className='qna-list-table-reception-number'>접수번호</div>
-                    <div className='qna-list-table-status'>
-                        <div className='primary-bedge'>접수</div>
-                    </div>
-                    <div className='qna-list-table-title' style={{ textAlign:'left' }}>제목</div>
-                    <div className='qna-list-table-writer-id'>작성자</div>
-                    <div className='qna-list-table-writer-date'>작성일</div>
-                    <div className='qna-list-table-viewcount'>조회수</div>
-                </div>
+                {viewList.map(item => <ListItem { ...item} />)}
             </div>
             <div className='qna-list-bottom'>
                 <div style={{ width: '299px' }}></div>
                 <div className='qna-list-pagenation'>
-                    <div className='qna-list-page-left'></div>
+                    <div className='qna-list-page-left' onClick={onPreSectionClickHandler}></div>
                     <div className='qna-list-page-box'>
-                        <div className='qna-list-page-active'>1</div>
-                        <div className='qna-list-page'>1</div>
+                        {pageList.map(page => 
+                        page === currentPage ? 
+                        <div className='qna-list-page-active'>{page}</div> :
+                        <div className='qna-list-page' onClick={() =>onPageClickHandler(page)}>{page}</div>
+                        )}
                     </div>
-                    <div className='qna-list-page-right'></div>
+                    <div className='qna-list-page-right' onClick={onNextSectionClickHandler}></div>
                 </div>
                 <div className='qna-list-search-box'>
                     <div className='qna-list-search-input-box'>
-                        <input className='qna-list-search-input'/>
+                        <input className='qna-list-search-input' placeholder='검색어를 입력하세요.' value={searchWord} onChange={onSearchWordChangeHandler}/>
                     </div>
-                    <div className='disable_button'>검색</div>
+                    <div className={searchButtonClass} onClick={onSearchButtonClickHandler}>검색</div>
                 </div>
             </div>
         </div>
